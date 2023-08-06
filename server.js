8ed1e5631d44a9860e8f674e181d997b25f8f54d
rau1avi1a <avila0118@icloud.com>
2023-08-06 15:29:31 -0700

    fixed GET routes

--- server.js
+++ server.js
@@ -5,26 +5,76 @@ const app = express();
 
 const PORT = 3001;
 
-const notes = require('./db/db.json')
+const notes = require('./db/db.json');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 app.use(express.static('public'));
 
-app.get('/', (req,res) =>
+app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/api/notes', (req,res) => {
+app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
-    console.info(`${req.method} request received to get notes`);
-
 });
+
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to get notes`);
+  
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading the file:', err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+  
+      try {
+        const notes = JSON.parse(data);
+        res.status(200).json(notes);
+      } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+  });
 
 app.post('/api/notes', (req,res) => {
     console.info(`${req.method} request received to add note`)
+    const newNote = req.body;
+    
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            return
+        }
+    
+        try {            
+            const jsonData = JSON.parse(data);
+
+            jsonData.push(newNote);
+
+            const updatedNotes = JSON.stringify(jsonData, null, 2);
+
+            fs.writeFile('./db/db.json', updatedNotes, 'utf8', (err) => {
+                if (err) {
+                  console.error('Error writing to the file:', err);
+                  return;
+                }
+                console.log('File updated successfully!');
+              });
+
+              res.status(200).sendFile(path.join(__dirname, '/public/notes.html'));
+            } catch (error) {
+              console.error('Error parsing JSON:', error);
+            }
+    })
+
 });
+
+app.delete('/api/notes/:id', (req, res) => {
+})
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
